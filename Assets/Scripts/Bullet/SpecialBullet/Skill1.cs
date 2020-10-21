ea409cdaf7d35ee7a5bd5025d22255e9a4bc9385
michael0703 <liaoweiskewer0703@gmail.com>
2020-10-21 15:43:15 +0800

    feat : 稻草人子彈可以生出一個稻草人(小花)

--- Assets/Scripts/Bullet/SpecialBullet/Skill1.cs
+++ Assets/Scripts/Bullet/SpecialBullet/Skill1.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Skill1 : SkillBase
-{
+{   
+    public GameObject scareCrowPrefab;
     protected override void ActivateSkill()
     {
-        Debug.Log("I have " + bullets.Length.ToString() + " bullets!");   
+           Debug.Log("Activate");
+           GameObject scareCrow = (GameObject)Instantiate(scareCrowPrefab);
+           scareCrow.transform.position = bullets[0].transform.position;
+
     }
 }
