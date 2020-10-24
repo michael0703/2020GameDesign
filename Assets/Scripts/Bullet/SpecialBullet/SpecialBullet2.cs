c59825c4565704b0d2f5d0d5f06ee0589039bbbd
b05902062 <b05902062@ntu.edu.tw>
2020-10-24 18:00:42 +0800

    fix : fix stash conflict

--- Assets/Scripts/Bullet/SpecialBullet/SpecialBullet2.cs
+++ Assets/Scripts/Bullet/SpecialBullet/SpecialBullet2.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class SpecialBullet2 : BulletBase
 {
-    protected override void OnTriggerEnter(Collider other)
+
+    protected void OnColliderEnter(Collider other)
     {
-        if(other.gameObject.tag=="Enemy")
-        {
-            other.gameObject.GetComponent<EnemyHealth>().GetHurt(damage);
-        }
-        if(other.gameObject.tag=="Wall")
+
+
+
+
+        if(other.gameObject.tag=="Floor")
         {
             isReady = true;
-            skill.GetComponent<SkillBase>().CheckBulletAreReady();
             rb.velocity = Vector3.zero;
+            skill.GetComponent<SkillBase>().CheckBulletAreReady();
+            
         }
         
     }
