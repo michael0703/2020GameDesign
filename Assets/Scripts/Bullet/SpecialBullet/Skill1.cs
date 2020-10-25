3ee2acbdb18eee2922cb1f424e0f2933419d28a1
michael0703 <liaoweiskewer0703@gmail.com>
2020-10-25 12:15:38 +0800

    fix : 修正技能1和技能5因為新增tag : "Floor"造成的bug

--- Assets/Scripts/Bullet/SpecialBullet/Skill1.cs
+++ Assets/Scripts/Bullet/SpecialBullet/Skill1.cs
@@ -6,11 +6,15 @@ public class Skill1 : SkillBase
 {   
     public GameObject scareCrowPrefab;
     protected override void ActivateSkill()
-    {
-           Debug.Log("Activate");
-           GameObject scareCrow = (GameObject)Instantiate(scareCrowPrefab);
-           scareCrow.transform.position = bullets[0].transform.position;
-
+    {       
+           if (bullets[0].GetComponent<SpecialBullet1>().collideObjectTag == 1){
+               Debug.Log("Activate");
+               GameObject scareCrow = (GameObject)Instantiate(scareCrowPrefab);
+               scareCrow.transform.position = bullets[0].transform.position;
+           }
+           else{
+                Debug.Log("Hit Wall, Nothing Happenend");
+           }
            DestroySkill();
     }
 }
