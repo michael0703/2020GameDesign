4de98c363975eae3e7e59df70536e2b8d36e69d1
michael0703 <liaoweiskewer0703@gmail.com>
2020-10-26 22:45:23 +0800

    feat: 1. 新增加 些許 劍的asset 2. 完成Enemy5，包含遠程攻擊

---
+++ Assets/Scripts/Enemy/SpecialMovement/Enemy5/EnemySpecialMovement5.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpecialMovement5 : EnemySpecialMovementBase
+{   
+    float rotateSpeed = 3f;
+    public GameObject swordRoundPrefab;
+    GameObject swordRound;
+    bool InitSwordRound = false;
+    public bool DestroySwordRound = false;
+
+    protected override void specialMove()
+    {
+        lookAtPlayer(player);
+        rb.velocity = transform.forward * 2f;
+
+        enemyAttackAction();
+
+    }
+    void enemyAttackAction(){
+
+        if(!InitSwordRound){
+            Vector3 position = gameObject.transform.Find("InnerCollider").transform.position;
+            position.y += 1f;
+            swordRound = (GameObject)Instantiate(swordRoundPrefab, position, Quaternion.identity);
+            swordRound.transform.parent = gameObject.transform;
+            // swordRound.name = "SwordRound";
+            InitSwordRound = true;
+        }
+        if(DestroySwordRound){
+            Destroy(swordRound);
+            InitSwordRound = false;
+            DestroySwordRound = false;
+        }
+
+    }
+    void lookAtPlayer(GameObject player)
+    {
+        Vector3 delta = player.transform.position - transform.position;
+        delta.y = 0;
+        // delta.x = 0;
+        Quaternion rotation = Quaternion.LookRotation(delta);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
+    }
+}
---
+++ Assets/Scripts/Enemy/SpecialMovement/Enemy5/SwordMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isAttacking = false;
+    public bool isReadyToDie = false;
+    float speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAttacking){
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            Debug.Log("Hit Player");
+            speed = 0f;
+            isReadyToDie = true;
+            GameObject player = GameObject.Find("/Player");
+            player.GetComponent<PlayerHealth>().GetHurt(50);
+            
+        }
+        if(other.gameObject.tag=="Wall" || other.gameObject.tag=="Floor")
+        {
+            Debug.Log("Hit Wall (Floor)");
+            speed = 0f;
+            isReadyToDie = true;
+        }
+    }
+}
