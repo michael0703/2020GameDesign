5d7f7b3c6a1cc10b3dbaf493d10d2554decf38ce
michael0703 <liaoweiskewer0703@gmail.com>
2020-11-18 15:54:24 +0800

    feat : 完成回力鏢子彈 + target的搭配

--- Assets/Scripts/Bullet/SpecialBullet/Skill5.cs
+++ Assets/Scripts/Bullet/SpecialBullet/Skill5.cs
@@ -5,15 +5,47 @@ using UnityEngine;
 public class Skill5 : SkillBase
 {   
     private GameObject player;
+    public bool isTargetCombo = false;
+    public GameObject skillManager;
+    public GameObject spBullet5_prefab;
+    public Transform targetTransform;
     protected override void ActivateSkill()
     {   
-        player = GameObject.Find("/Player/MainCamera/Gun");
-        for(int i = 0; i < bullets.Length; i++){
-            Vector3 velocity = player.transform.position - bullets[i].gameObject.transform.position;
-            bullets[i].GetComponent<Rigidbody>().velocity = velocity * 3f;
-            bullets[i].GetComponent<SpecialBullet5>().isWaitingToActivate = true;
+        if (isTargetCombo){
+            activateTargetCombo();
+            DestroySkill();
+        }
+        else{
+            player = GameObject.Find("/Player/MainCamera/Gun");
+            for(int i = 0; i < bullets.Length; i++){
+                Vector3 velocity = player.transform.position - bullets[i].gameObject.transform.position;
+                bullets[i].GetComponent<Rigidbody>().velocity = velocity * 3f;
+                bullets[i].GetComponent<SpecialBullet5>().isWaitingToActivate = true;
+            }
         }
     }
+    void activateTargetCombo(){
+        skillManager = GameObject.Find("SkillManager");
+        int cloneBulletNum = 5;
+        for(int i=0; i<cloneBulletNum; i++){
+            Vector3 initPos = targetTransform.position;
+            initPos.y +=  targetTransform.localScale.y * 0.5f * 0.75f;
+            float eulerRotationY = (360 / cloneBulletNum) * i;
+            Vector3 tmp = Vector3.zero;
+            tmp.x = Mathf.Cos(Mathf.Deg2Rad*eulerRotationY);
+            tmp.z = Mathf.Sin(Mathf.Deg2Rad*eulerRotationY);
+            tmp = tmp.normalized * targetTransform.localScale.y * 0.9f;
+            initPos.x += tmp.x;
+            initPos.z += tmp.z;
+            Quaternion initRotate = Quaternion.Euler(0, eulerRotationY, 0);
+            GameObject specialBullet = (GameObject)Instantiate(spBullet5_prefab, initPos, Quaternion.identity);
+            specialBullet.transform.forward = new Vector3(tmp.x, 0, tmp.z);
+            specialBullet.GetComponent<SpecialBullet5>().isCloneBullet = true;
+            bool isFull = skillManager.GetComponent<SkillManager>().RegistBullet(specialBullet);
+        }        
+    }
+
+    
     void Update(){
         bool flag = true;
         for(int i=0 ;i<bullets.Length; i++){
