078e31a808a1747dcc2c62beb71453da19b35140
michael0703 <liaoweiskewer0703@gmail.com>
2020-12-06 01:01:10 +0800

    feat : 射擊時加入傷害顯示，傷害強度也會改變字體大小

---
+++ Assets/Scripts/Enemy/EnemyDamagePopUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamagePopUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float existTime = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        existTime += Time.deltaTime;
+        if (existTime > 0.5f){
+            Object.Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyHealth : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class EnemyHealth : MonoBehaviour
     public int startHealth = 100;
     public int currentHealth;
     public AnimationClip deadAnim;
-
+    public GameObject damagePopUpPrefab;
     private GameObject player;
     private Rigidbody rb;
     private bool isMovable;
@@ -26,7 +27,22 @@ public class EnemyHealth : MonoBehaviour
         // if (isMovable){
         //     rb.AddForce(-1f * transform.forward * 100f);
         // }
+        for(int i=0; i < this.gameObject.transform.childCount; i++){
+            GameObject tmp = this.gameObject.transform.GetChild(i).gameObject;
+            if (tmp.layer == 5){
+                // this is health canvas
+                GameObject healthbar = tmp.transform.GetChild(0).gameObject;
+                Vector3 damagePos = healthbar.transform.position;
+                damagePos.y += Random.Range(-0.2f, 0.2f);
+                GameObject dmgPop = (GameObject)Instantiate(damagePopUpPrefab, damagePos, Quaternion.identity);
+                dmgPop.GetComponent<TextMeshPro>().SetText(damage.ToString());
+                dmgPop.GetComponent<TextMeshPro>().fontSize = 3 * (1f + (damage / 5f) * 0.01f);
+                dmgPop.transform.rotation = healthbar.transform.rotation;
+            }
+        }
         
+        
+
         currentHealth -= damage;
         Debug.Log("Enemy health: " + currentHealth);
         if(currentHealth <= 0)
