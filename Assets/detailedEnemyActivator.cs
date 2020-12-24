baa3791b5908b84b5e13cb4c06c3b5edadd44af9
b05902062 <b05902062@ntu.edu.tw>
2020-12-25 00:44:58 +0800

    add raycast to player

---
+++ Assets/detailedEnemyActivator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detailedEnemyActivator : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
