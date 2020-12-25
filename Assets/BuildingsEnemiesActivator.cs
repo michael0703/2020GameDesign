fa48fa8fa50b9ae1a754db3303b7de97644c927a
b05902062 <b05902062@ntu.edu.tw>
2020-12-25 13:43:04 +0800

    try to make level2 faster

---
+++ Assets/BuildingsEnemiesActivator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingsEnemiesActivator : MonoBehaviour
+{
+
+    static private GameObject buildings;
+    static private GameObject enemies;
+
+    private void Start()
+    {
+        buildings = GameObject.Find("===Buildings===");
+        enemies = GameObject.Find("===Enemys===");
+        setEnvironment(0);
+
+    }
+
+    public static void setEnvironment(int index)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if (i == index )
+            {
+                buildings.transform.GetChild(i).gameObject.SetActive(true);
+                enemies.transform.GetChild(i).gameObject.SetActive(true);
+
+                buildings.transform.GetChild(i).GetChild(3).gameObject.SetActive(true);
+
+            }
+            else if(i == index + 1)
+            {
+                buildings.transform.GetChild(i).gameObject.SetActive(true);
+                enemies.transform.GetChild(i).gameObject.SetActive(true);
+
+                buildings.transform.GetChild(i).GetChild(3).gameObject.SetActive(false);
+
+
+            }
+            else
+            {
+                buildings.transform.GetChild(i).gameObject.SetActive(false);
+                enemies.transform.GetChild(i).gameObject.SetActive(false);
+            }
+
+
+        }
+
+
+    }
+
+
+
+}
